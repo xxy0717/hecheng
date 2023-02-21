52ac8c54a9f13d16d133b75e2c7bab42f4ae254d
xxy0717 <125196979+xxy0717@users.noreply.github.com>
2023-02-21 15:16:41 +0800

    Update hecheng.py

--- hecheng.py
+++ hecheng.py
@@ -1,70 +1,36 @@
-python -m pip install --upgrade pip
-
 import streamlit as st
-from PIL import Image, ImageFilter
-import requests
-from io import BytesIO
+from PIL import Image, ImageOps
 import numpy as np
-import cv2
 
+# 设置页面标题和头部
+st.set_page_config(page_title='孩子头像生成器', page_icon=':baby:', layout='wide')
 
-@st.cache
-def load_image(image_file):
-    img = Image.open(image_file)
-    return img
+st.title("孩子头像生成器")
 
+# 定义上传照片函数
+def upload_image(name):
+    uploaded_image = st.file_uploader(f"上传{name}照片", type=['jpg', 'jpeg', 'png'])
+    if uploaded_image is not None:
+        image = Image.open(uploaded_image)
+        st.image(image, caption=f"{name}照片", use_column_width=True)
+        return image
 
-def image_to_array(image):
-    img_array = np.array(image.convert("RGB"))
-    img_array = img_array[:, :, ::-1].copy()
-    return img_array
+# 上传父亲和母亲的照片
+father_image = upload_image("父亲")
+mother_image = upload_image("母亲")
 
+# 如果上传了两张照片
+if father_image is not None and mother_image is not None:
 
-def array_to_image(img_array):
-    img = Image.fromarray(np.uint8(img_array))
-    return img
+    # 调整父亲和母亲的图像大小和模式
+    father_image = father_image.convert('RGB').resize((256, 256))
+    mother_image = mother_image.convert('RGB').resize((256, 256))
 
+    # 按比例合成孩子头像
+    alpha = np.random.uniform(0.2, 0.8)  # 随机选择 alpha 值
+    child_array = np.array(alpha * np.array(father_image) + (1 - alpha) * np.array(mother_image), dtype=np.uint8)
+    child_image = Image.fromarray(child_array)
 
-def morphing(father_image, mother_image, child_ratio=0.5):
-    father_array = image_to_array(father_image)
-    mother_array = image_to_array(mother_image)
-
-    assert father_array.shape == mother_array.shape
-
-    mask = np.zeros(father_array.shape, dtype=np.float32)
-    mask[:, :int(father_array.shape[1]*child_ratio), :] = 1
-
-    father_mask = father_array * mask
-    mother_mask = mother_array * (1 - mask)
-
-    child_array = father_mask + mother_mask
-    child_image = array_to_image(child_array)
-
-    return child_image
-
-
-def main():
-    st.title("Generate a Child's Photo")
-
-    st.write("Please upload photos of father and mother.")
-
-    father_image = st.file_uploader("Father's Photo", type=["jpg", "jpeg", "png"])
-    mother_image = st.file_uploader("Mother's Photo", type=["jpg", "jpeg", "png"])
-
-    if father_image and mother_image:
-        col1, col2 = st.beta_columns(2)
-        with col1:
-            st.write("Father's Photo")
-            st.image(father_image, use_column_width=True)
-
-        with col2:
-            st.write("Mother's Photo")
-            st.image(mother_image, use_column_width=True)
-
-        if st.button("Generate"):
-            child_image = morphing(father_image, mother_image)
-            st.image(child_image, use_column_width=True)
-
-
-if __name__ == "__main__":
-    main()
+    # 显示合成的孩子头像
+    st.header("合成的孩子头像：")
+    st.image(child_image, caption="孩子照片", use_column_width=True)
